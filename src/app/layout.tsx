a3d8ae67852249bb3c0a9861c6f2d52151137584
telecasteren <127958662+telecasteren@users.noreply.github.com>
2026-02-23 21:47:59 +0100

    Added data-scroll attribute to html element so next.js sees its intended usage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-scroll-behavior="smooth">
       <body className={robotoCondensed.variable}>
         <AppRouterCacheProvider>
           <MuiThemeProvider>{children}</MuiThemeProvider>
